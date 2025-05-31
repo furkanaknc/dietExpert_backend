66bc37c75455a38be60e1b4ae3ccc59c755b03ac
Furkan Akıncı <furkanaknc55@gmail.com>
2025-06-01 02:28:35 +0300

    feat: add database structure

---
+++ prisma/migrations/20250531112341_add_new_ai_features/migration.sql
@@ -0,0 +1,59 @@
+-- CreateEnum
+CREATE TYPE "MessageRole" AS ENUM ('USER', 'ASSISTANT');
+
+-- CreateEnum
+CREATE TYPE "ContentType" AS ENUM ('TEXT', 'AUDIO', 'IMAGE', 'MIXED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Chat" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" TEXT NOT NULL,
+    "chat_id" TEXT NOT NULL,
+    "role" "MessageRole" NOT NULL,
+    "content_type" "ContentType" NOT NULL DEFAULT 'TEXT',
+    "content" JSONB NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Chat_user_id_idx" ON "Chat"("user_id");
+
+-- CreateIndex
+CREATE INDEX "Message_chat_id_idx" ON "Message"("chat_id");
+
+-- CreateIndex
+CREATE INDEX "Message_created_at_idx" ON "Message"("created_at");
+
+-- AddForeignKey
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_chat_id_fkey" FOREIGN KEY ("chat_id") REFERENCES "Chat"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/migrations/20250531155119_create_user_status_enum/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - Added the required column `first_name` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `last_name` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "name",
+ADD COLUMN     "first_name" TEXT NOT NULL,
+ADD COLUMN     "last_name" TEXT NOT NULL,
+ADD COLUMN     "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE';
