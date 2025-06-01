b08a830a8762b3a15bae8e1a0a39cc82e3535ac8
Furkan Akıncı <furkanaknc55@gmail.com>
2025-06-01 03:05:58 +0300

    fix: validation-pipe issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, ValidationPipe } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { EnvironmentModule } from './modules/common/environments/environment.module';
 import { PrismaModule } from './modules/common/prisma/prisma.module';
 import { AIModule } from './modules/ai/ai.module';
@@ -8,6 +8,7 @@ import { GlobalExceptionFilter } from './common/exception-filters/global-excepti
 import { JwtGuard } from './common/guards/jwt.guard';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ValidationPipe } from './common/pipes/validation.pipe';
 
 @Module({
   imports: [EnvironmentModule, PrismaModule, AIModule, ChatModule, UsersModule, AuthModule],
